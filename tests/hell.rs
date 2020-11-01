75cc72d4acd36f82b08990bf88b6bb97e2e2d0ab
Lauri Gustafsson <me@gustafla.space>
2020-11-01 22:11:39 +0200

    Clean up

--- tests/hell.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let output = std::process::Command::new("./hello1").output().unwrap();
-    let string = String::from_utf8(output.stdout).unwrap();
-    assert_eq!(string, "Hello");
-}
