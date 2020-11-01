7801868662e09fb190621aa811aff43d9ba38ef0
Lauri Gustafsson <me@gustafla.space>
2020-11-01 20:40:06 +0200

    Fix stuff using git add -p

--- tests/hell.rs
+++ tests/hell.rs
@@ -1,5 +1,5 @@
 fn main() {
     let output = std::process::Command::new("./hello1").output().unwrap();
     let string = String::from_utf8(output.stdout).unwrap();
-    assert_eq!(string, "Hello\n");
+    assert_eq!(string, "Hello");
 }
