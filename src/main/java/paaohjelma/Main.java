bcace5288bc3461444bc9cecdc800f6ff1eb0759
Lauri Gustafsson <me@gustafla.space>
2020-11-09 23:09:14 +0200

    2.2 Break every checkstyle rule

--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -4,9 +4,23 @@ import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        if (true == false) {
+            if (true == true) {
+                if (false == false) {
+                    System.out.println("Ei voi olla");
+                }
+            }
+        }
 
-        Varasto mehua = new Varasto(100.0);
+        for( int i=0; i<1; i++ ) {
+            for( int j=0; i<j; j++ ) {
+                System.out.println("virhe");
+            }
+        }
+
+            Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
         System.out.println("Luonnin jälkeen:");
