16296d335624ba0be5859571cfe6f296a4738ced
Lauri Gustafsson <me@gustafla.space>
2020-11-09 23:22:27 +0200

    2.2. Break checkstyle for github actions

--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -4,7 +4,8 @@ import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Varasto olutta = new Varasto(100.0, 20.2);
 
         System.out.println("Luonnin jälkeen:");
